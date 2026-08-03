b90980799822c59c2131501b3b87722785188680
Kolosi <laurequezian03@gmail.com>
2026-08-03 19:19:34 +0800

    Rename Orders to Purchases and adjust fields
    Renamed Orders table to Purchases and updated relevant fields.

--- Purchases.sql
+++ Purchases.sql
@@ -1,9 +1,9 @@
-CREATE TABLE Orders (
+CREATE TABLE Purchases (
 
-  order_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  purchase_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   client_id INT NOT NULL,
   item_id INT NOT NULL,
-  order_date DATE NOT NULL,
+  purchase_date DATE NOT NULL,
   CONSTRAINT fk_client
     FOREIGN KEY (client_id)
   	REFERENCES Clients(client_id),
@@ -13,7 +13,7 @@ CREATE TABLE Orders (
   
 );
 
-INSERT INTO Orders (order_id, client_id, item_id, order_date)
+INSERT INTO Purchases(purchase_id, client_id, item_id, purchase_date)
 VALUES 	 (1, 113, 1002, '2026-08-03'),
 	     (2, 104, 1011, '2026-08-03'),
 	     (3, 110, 1007, '2026-08-03'),
