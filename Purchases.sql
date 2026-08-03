d8882f13539984527b6afaeab73cd5be342ff3d5
Kolosi <laurequezian03@gmail.com>
2026-08-03 20:15:39 +0800

    Add example SQL queries for joins and conditions
    Added example queries demonstrating joins and conditional logic.

--- Purchases.sql
+++ Purchases.sql
@@ -31,3 +31,47 @@ FROM Purchases
 
 LEFT JOIN Clients ON Clients.client_id = Purchases.client_id
 LEFT JOIN Items ON Items.item_id = Purchases.item_id;
+
+/* Just an example */
+
+SELECT	 Clients.client_surname,
+		Clients.city,
+		Items.item_name,
+		Items.category,
+		Purchases.purchase_date
+
+FROM Purchases
+
+INNER JOIN Clients ON Clients.client_id = Purchases.client_id
+INNER JOIN Items ON Items.item_id = Purchases.item_id;
+
+
+/* Simple Join Query with IF and ELSE statement */
+
+
+Select 	Items.item_name,
+		Items.price,
+
+Case
+	WHEN Items.price = 120 THEN 'YES'
+    WHEN Items.price > 120 THEN 'NO'
+    ELSE 'LESS THAN 120'
+
+END AS 'IS IT 120?'
+    
+FROM Purchases
+
+LEFT JOIN Items ON Items.item_id = Purchases.item_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
